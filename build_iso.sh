f2a2294401cea41367fb7364e55aa04817f9a127
cei35 <hackingtoolsservice@gmail.com>
2025-10-28 21:13:15 +0100

    fix(build) : missing zlogin and wrong src/dst files when copying

--- build_iso.sh
+++ build_iso.sh
@@ -12,17 +12,25 @@
 # TODO:
 # Add MBR install AND Server version (with SSH/Auditd...)
 
+set -e
+
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
 echo "[+] Building ArchInstall ISO..."
 
 echo "[+] Installing archiso package if needed..."
 sudo pacman -Sy --noconfirm --needed archiso
 
+echo ""
 echo "[+] Setting up build environment..."
 mkdir -p build
 cd build
 
 echo "[+] Copying releng profile..."
-rm -rf iso/
+rm -rf iso/ out/ work/
 cp -r /usr/share/archiso/configs/releng/ iso/
 
 echo "[+] Adding dialog packages..."
@@ -33,24 +41,25 @@ grep -qF "dialog" iso/packages.x86_64 || {
     sort -o iso/packages.x86_64 iso/packages.x86_64
 }
 
-echo "[+] Select type :
+echo ""
+echo "[+] Select ISO type to build:
 1. UEFI Core Only
 2. UEFI GUI (only Xfce4, Hyprland or Gnome for now)
 "
 
 read -n 1 -rp "[?] Choose number : " choice
 
+cp $PWD/scripts/core/core_install_uefi.sh iso/airootfs/root/install.sh
+
 case $choice in
 1)
     echo "[+] Setting up UEFI Core Only ISO..."
-    cp ../scripts/core/core_install_uefi.sh iso/airootfs/scripts/install.sh
-    cp ../scripts/core/core_post_install_uefi.sh iso/airootfs/scripts/post_install.sh
+    cp $PWD/scripts/core/core_post_install_uefi.sh iso/airootfs/root/post_install.sh
     mode="core"
     ;;
 2)
     echo "[+] Setting up UEFI GUI ISO..."
-    cp ../scripts/gui/gui_install_uefi.sh iso/airootfs/scripts/install.sh
-    cp ../scripts/gui/gui_post_install_uefi.sh iso/airootfs/scripts/post_install.sh
+    cp $PWD/scripts/gui/gui_post_install_uefi.sh iso/airootfs/root/post_install.sh
     mode="gui"
     ;;
 *)
@@ -59,11 +68,21 @@ case $choice in
     ;;
 esac
 
+chmod +x iso/airootfs/root/*.sh
+
+echo "
+chmod +x ~/install.sh
+~/install.sh" >> iso/airootfs/root/.zlogin
+
 echo "[+] Building ISO..."
 mkdir -p work out
+
+t0=$(date +%s)
+
 mkarchiso -v -w work -o out iso
+t1=$(date +%s)
 
 ISO="archinstall_${mode}_$(date +%Y-%m-%d).iso"
 mv out/*.iso "$ISO"
 
-echo "[+] build complete: $ISO"
+echo "[+] build complete: $ISO (in $((t1 - t0)) seconds)"
